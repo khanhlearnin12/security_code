589960909852cd8e8c32faef10c0480b2075e355
khanhlearnin12 <hakhanh12.study@gmail.com>
2025-10-02 18:10:34 +0800

    update finish

--- ceaser.c
+++ ceaser.c
@@ -6,6 +6,37 @@
 #define max_length 500
 #define max_key 50
 
+void autokey_encrpyt(char *planttext, char *key, char* ciphertext){
+    int p_len = strlen(planttext);
+    int k_len = strlen(key);
+
+    //check if the output is larger enough
+    if (p_len >= max_length) 
+    {
+        printf("plaintext is so lonng noooooo");
+        return;
+    }
+
+    for (int i = 0; i < p_len; i++)
+    {
+        //convert from the plaintext to Number 
+        int p_val = toupper(planttext[i]) - 'A';
+        int k_val;
+
+        if (i < k_len)
+        {
+            k_val = toupper(key[i]) - 'A';
+        } else {
+            k_val = toupper(planttext[i - k_len]) - 'A';
+        }
+        int c_val = (p_val + k_val) % 26; 
+
+        ciphertext[i] = c_val + 'A';
+    }
+    ciphertext[p_len] = '\0';
+}
+
+
 void autokey_decrypt(char *ciphertext, char *key, char* planttext){
     int c_len = strlen(ciphertext);
     int k_len = strlen(key);
@@ -42,8 +73,16 @@ void autokey_decrypt(char *ciphertext, char *key, char* planttext){
 int main(){
     char ciphertext[max_length], key[max_key], planttext[max_length];
     
-    char user_choise[max_length] = fgets(user_choise,max_length,stdin);
-    if ( user_choise == "decrypt")
+    char user_choise[max_length];
+    printf("Do you want to encrypt or decrypt:");
+    if (fgets(user_choise,max_length,stdin)== NULL )
+    {
+        printf("you are not input into it ");
+        return 1;
+    }
+    user_choise[strcspn(user_choise,"\n")] = 0;
+
+    if (strcmp(user_choise,"decrypt") == 0)
     {
         printf("Input the ciphertext:");
         if (fgets(ciphertext,max_length,stdin)== NULL)  
@@ -64,9 +103,10 @@ int main(){
 
         printf("%s\n",planttext);
 
-    } else if (user_choise == "encrypt"){
+    } 
+    else if (strcmp(user_choise,"encrypt") == 0){
 
-        printf("Input the ciphertext:");
+        printf("Input the planttext:");
         if (fgets(ciphertext,max_length,stdin)== NULL)  
         {
             printf("you are not input into it ");
@@ -81,8 +121,11 @@ int main(){
             return 1;
         }
         planttext[strcspn(planttext,"\n")] = 0;
-        autokey_decrypt(ciphertext,key,planttext);
-    } else {
+        autokey_encrpyt(ciphertext,key,planttext);
+
+        printf("%s\n",planttext);
+    } 
+    else {
         printf("No input of user");
         return 1;
     }
